c8f6b2e71ab56e4f9635aff2ffbbda344ecda778
JacobPineda24 <jafet.pinedaramirez@cesunbc.edu.mx>
2025-10-22 14:14:56 -0700

    Refactor app.run to use a variable for port

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ def success_premium():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=False)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
